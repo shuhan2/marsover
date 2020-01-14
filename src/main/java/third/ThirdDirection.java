c3ee8f41a89508beccc87d96130df30c9a102f24
qianwu2 <qianwu2@ap.xom.com>
2020-01-14 14:30:17 +0800

    marsover-3-turnRight[qian] do some refactor for enum

--- src/main/java/third/ThirdDirection.java
+++ src/main/java/third/ThirdDirection.java
@@ -3,10 +3,11 @@ package third;
 import java.util.function.Function;
 
 public enum ThirdDirection {
-  NORTH(getAddYFunction()),
-  EAST(getAddXFunction()),
-  SOUTH(getSubYFunction()),
-  WEST(getSubXFunction());
+  NORTH(Constants.ADD_Y_FUNCTION),
+  EAST(Constants.ADD_X_FUNCTION),
+  SOUTH(Constants.SUB_Y_FUNCTION),
+  WEST(Constants.SUB_X_FUNCTION);
+
   private Function<ThirdMarsOver, ThirdMarsOver> goForward;
 
   public Function<ThirdMarsOver, ThirdMarsOver> getGoForward() {
@@ -17,19 +18,11 @@ public enum ThirdDirection {
     this.goForward = goForward;
   }
 
-  private static Function<ThirdMarsOver, ThirdMarsOver> getAddXFunction() {
-    return marsOVer -> new ThirdMarsOver(marsOVer.getX() + 1, marsOVer.getY(), marsOVer.getDirection());
-  }
+  private static class Constants {
 
-  private static Function<ThirdMarsOver, ThirdMarsOver> getSubXFunction() {
-    return marsOVer -> new ThirdMarsOver(marsOVer.getX() - 1, marsOVer.getY(), marsOVer.getDirection());
-  }
-
-  private static Function<ThirdMarsOver, ThirdMarsOver> getAddYFunction() {
-    return marsOVer -> new ThirdMarsOver(marsOVer.getX(), marsOVer.getY() + 1, marsOVer.getDirection());
-  }
-
-  private static Function<ThirdMarsOver, ThirdMarsOver> getSubYFunction() {
-    return marsOVer -> new ThirdMarsOver(marsOVer.getX(), marsOVer.getY() - 1, marsOVer.getDirection());
+    static final Function<ThirdMarsOver, ThirdMarsOver> SUB_Y_FUNCTION = marsOVer -> new ThirdMarsOver(marsOVer.getX(), marsOVer.getY() - 1, marsOVer.getDirection());
+    static final Function<ThirdMarsOver, ThirdMarsOver> SUB_X_FUNCTION = marsOVer -> new ThirdMarsOver(marsOVer.getX() - 1, marsOVer.getY(), marsOVer.getDirection());
+    static final Function<ThirdMarsOver, ThirdMarsOver> ADD_X_FUNCTION = marsOVer -> new ThirdMarsOver(marsOVer.getX() + 1, marsOVer.getY(), marsOVer.getDirection());
+    static final Function<ThirdMarsOver, ThirdMarsOver> ADD_Y_FUNCTION = marsOVer -> new ThirdMarsOver(marsOVer.getX(), marsOVer.getY() + 1, marsOVer.getDirection());
   }
 }
